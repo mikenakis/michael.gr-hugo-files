3c9b11d176e7558824280642d5779732d7657c0e
MikeNakis <git@michael.gr>
2025-10-01 01:04:33 +0200

    Work-in-Progress 2025-10-01  1:04:33.64

--- s.bat
+++ s.bat
@@ -1,6 +1,6 @@
 @echo off
 set source=%CD%
 hugo server --buildDrafts --cleanDestinationDir --buildFuture --minify --navigateToChanged --panicOnWarning --source %source%
-:: --printPathWarnings
-:: --printUnusedTemplates cannot use because the stack theme issues such a warning
+:: --printPathWarnings    cannot use because these warnings keep randomly popping up.
+:: --printUnusedTemplates cannot use because the stack theme issues such a warning.
 :: --disableFastRender
