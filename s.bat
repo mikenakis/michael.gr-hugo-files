10f155b76477c38441a1f27169f9c6377c86dcd3
MikeNakis <git@michael.gr>
2025-10-04 20:39:12 +0200

    added _gen to .gitignore

--- s.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set source=%CD%
-hugo server --buildDrafts --cleanDestinationDir --buildFuture --minify --navigateToChanged --panicOnWarning --source %source%
-:: --printPathWarnings    cannot use because these warnings keep randomly popping up.
-:: --printUnusedTemplates cannot use because the stack theme issues such a warning.
-:: --disableFastRender
